b911762c8a9dc2c849a9063b0168e5a1ed527c74
gustavo <gu_kiub@hotmail.com>
2020-01-15 15:22:14 -0300

    up_C#

---
+++ C#/Loja/Loja/interfaces/Imposto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mercadao.interfaces
+{
+    interface Imposto
+    {
+        public abstract double calculaImposto();
+    }
+}
