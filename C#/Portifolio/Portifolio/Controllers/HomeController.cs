1645cf8483ec6270a6079b1284b4b1c553481a2e
gustavo <gu_kiub@hotmail.com>
2020-01-23 15:04:05 -0300

    Site_10

---
+++ C#/Portifolio/Portifolio/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portifolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Projetos()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contato()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Skills() 
+        {
+            ViewBag.Message = "Your skills description page.";
+
+                return View();
+        }
+    }
+}
