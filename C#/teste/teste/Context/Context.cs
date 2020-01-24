d4d3d99d562d36bf8c09350b59f9f575f160c51c
gustavo <gu_kiub@hotmail.com>
2020-01-24 15:00:14 -0300

    batata

---
+++ C#/teste/teste/Context/Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using teste.Models;
+
+namespace teste.Context
+{
+    public class Aula1Context : DbContext
+    {
+        public Aula1Context() 
+            : base("conn")
+        {
+            
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ C#/teste/teste/Models/Cliente.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace teste.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(150, ErrorMessage = "Maximo de caracteres excedido")]
+        [MinLength(2, ErrorMessage = "Minimo de 2 caracteres")]
+        [DisplayName("Nome do Cliente")]
+        [Required(ErrorMessage = "Preencher campo nome")]
+        public string Nome { get; set; }
+
+        [MaxLength(150, ErrorMessage = "Maximo de caracteres excedido")]
+        [MinLength(2, ErrorMessage = "Minimo de 2 caracteres")]
+        [DisplayName("Nome do Sobrenome")]
+        [Required(ErrorMessage = "Preencher campo sobrenome")]
+        public string SobreNome { get; set; }
+        [ScaffoldColumn(false)]
+
+        public DateTime DataCadastro { get; set; }
+        [MaxLength(150, ErrorMessage = "Maximo de caracteres excedido")]
+        [MinLength(2, ErrorMessage = "Minimo de 2 caracteres")]
+        [DisplayName("Nome do E-mail")]
+        [Required(ErrorMessage = "Preencher campo E-mail")]
+        [EmailAddress(ErrorMessage = "E-mail em formato invalido")]
+        public string Email { get; set; }
+
+    }
+}
