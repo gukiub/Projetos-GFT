9df65e9c82790cd17fef88e9c842062d12d62133
gukiub <gu_kiub@hotmail.com>
2020-03-17 15:09:12 -0300

    projeto_de_java

---
+++ JavaScript/fetchJS/fetch.js
@@ -0,0 +1,8 @@
+const url = 'https://pokeapi.co/api/v2/pokemon';
+
+fetch(url)
+.then(response => response.json())
+.then(data => {
+    console.log(data)
+})
+.catch(error => console.log(console.log(error)));
